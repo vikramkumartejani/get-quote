e6a73390ac39af958c66314e137ab2d6669cb119
vikram kumar <vikramkumar8t8@gmail.com>
2024-08-29 00:43:31 +0500

    'update'

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="w-full max-w-[1440px] mx-auto px-6 py-10">
+    <div className="w-full max-w-[1440px] mx-auto px-8 sm:px-10 md:px-20 py-10 ">
         <div className="w-full flex items-center lg:flex-row flex-col justify-between lg:gap-5 gap-10">
-            <div className="lg:text-left text-center flex flex-col lg:items-start items-center gap-3 sm:gap-5 lg:max-w-[600px] 2xl:max-w-[700px]">
-                <h1 className="text-2xl sm:text-3xl font-medium mb-2 flex flex-col gap-2">
+            <div className="max-w-[350px] w-full lg:text-left text-center flex flex-col lg:items-start items-center gap-3 sm:gap-5">
+                <h1 className="text-[33px] sm:text-[36px] font-medium mb-2 flex flex-col gap-2 tracking-normal">
                     Lorem ipsum dolar
-                    <span className="font-orbitron text-2xl sm:text-3xl">sit amet</span>
+                    <span className="font-orbitron text-3xl">sit amet</span>
                 </h1>
-                <p className="text-base sm:text-xl 2xl:text-2xl font-normal">
+                <p className=" text-xs font-normal">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident ratione numquam amet.
                 </p>
-                <p className="text-base sm:text-xl 2xl:text-2xl font-normal">
+                <p className="text-xs font-normal">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda officia delectus ullam, labore perspiciatis incidunt quibusdam architecto deserunt facilis cumque.
                 </p>
-                <button className="mt-3 uppercase bg-[#441109] text-white w-[160px] md:w-[180px] h-[60px] md:h-[70px] text-sm sm:text-base 2xl:text-lg tracking-[0.2em] font-light">Get Quote</button>
+                <button className="mt-3 uppercase bg-[#441109] text-white w-[160px] h-[60px] md:h-[55px] text-sm sm:text-base 2xl:text-lg tracking-[0.2em] font-light">Get Quote</button>
             </div>
-            <div className="flex items-end relative mb-10 md:mb-0">
+            <div className="flex items-end relative mb-10 md:mb-0 ">
                 <img src="./assets/image.jpg" alt="image" className="sm:min-w-[200px] max-h-[200px] sm:max-h-full sm:h-[300px] -mb-[100px] -mr-[50px] sm:-mr-[100px] z-10" />
                 <img src="./assets/image.jpg" alt="image" className="min-w-[215px] sm:min-w-[250px] max-h-[300px] sm:max-h-[400px]" />
             </div>
